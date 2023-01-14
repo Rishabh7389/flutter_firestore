d15f807da344e47b9940f847cd04bbbe3d2ebd2d
RISHABH KUMAR <rishabhkumar7389@gmail.com>
2023-01-14 22:45:24 +0530

    commit

--- lib/HomePage.dart
+++ lib/HomePage.dart
@@ -7,10 +7,12 @@ class HomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-          title: Text(
-        "DATA INPUT",
-        style: TextStyle(color: Colors.white),
-      )),
+        title: Text(
+          "DATA INPUT",
+          style: TextStyle(color: Colors.white),
+        ),
+        centerTitle: true,
+      ),
       drawer: Drawer(),
     );
   }
